36d5b8e930e4939271fc3db8b60c39ac5f896594
Peixin Li <pxli@nyu.edu>
2022-01-19 13:21:47 +0800

    fix

--- .github/workflows/action-helper/python/utils.py
+++ .github/workflows/action-helper/python/utils.py
@@ -164,3 +164,21 @@ class EnvDefault(argparse.Action):
 
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, values)
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    this function is copied from distutils.util to avoid deprecation waring https://www.python.org/dev/peps/pep-0632/
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
